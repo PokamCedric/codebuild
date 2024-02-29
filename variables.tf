7dbf3574b6b7f7d171aed155ad45f636bba26e72
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-03-01 00:09:24 +0100

    Added variables default values

--- variables.tf
+++ variables.tf
@@ -2,21 +2,25 @@
 # Environment
 ################################################################################
 variable "region" {
+  default     = "us-east-1"
   description = "Region we need to deploy the infrastructure"
   type        = string
 }
 
 variable "profile" {
+  default     = "terraform-user"
   description = "AWS Profile user"
   type        = string
 }
 
 variable "project_name" {
+  default     = "techmax"
   description = "Project Name"
   type        = string
 }
 
 variable "environment" {
+  default     = "Dev"
   description = "Project Environment"
   type        = string
 }
